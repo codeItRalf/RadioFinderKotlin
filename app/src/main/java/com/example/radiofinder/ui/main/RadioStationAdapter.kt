07b0ac318259c932abc6492a28899c7eb8f82f69
codeItRalf <raffe_83@hotmail.com>
2024-05-26 05:33:34 +0200

    made some adjustments for radio items

--- app/src/main/java/com/example/radiofinder/ui/main/RadioStationAdapter.kt
+++ app/src/main/java/com/example/radiofinder/ui/main/RadioStationAdapter.kt
@@ -3,22 +3,27 @@ package com.example.radiofinder.ui.main
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.radiofinder.R
 import com.example.radiofinder.data.model.RadioStation
+import com.squareup.picasso.Picasso
 
-class RadioStationAdapter(private val clickListener: (RadioStation) -> Unit) :
-    RecyclerView.Adapter<RadioStationAdapter.ViewHolder>() {
+class RadioStationAdapter(private val clickListener: (RadioStation) -> Unit) : RecyclerView.Adapter<RadioStationAdapter.ViewHolder>() {
 
     private var stations: List<RadioStation> = listOf()
 
-    class ViewHolder(view: View, private val clickListener: (RadioStation) -> Unit) :
-        RecyclerView.ViewHolder(view) {
+    class ViewHolder(view: View, private val clickListener: (RadioStation) -> Unit) : RecyclerView.ViewHolder(view) {
         private val nameTextView: TextView = view.findViewById(R.id.station_name)
+        private val descriptionTextView: TextView = view.findViewById(R.id.station_description)
+        private val stationImageView: ImageView = view.findViewById(R.id.station_image)
 
         fun bind(station: RadioStation) {
             nameTextView.text = station.name
+            descriptionTextView.text = station.country // or any other detail you want to display
+            Picasso.get().load(station.favicon).into(stationImageView)
+
             itemView.setOnClickListener {
                 clickListener(station)
             }
@@ -37,7 +42,7 @@ class RadioStationAdapter(private val clickListener: (RadioStation) -> Unit) :
     override fun getItemCount(): Int = stations.size
 
     fun submitList(newStations: List<RadioStation>) {
-        stations = newStations
+        stations = newStations.filter { !it.name.isNullOrBlank() && !it.resolvedUrl.isNullOrBlank() }
         notifyDataSetChanged()
     }
 }
