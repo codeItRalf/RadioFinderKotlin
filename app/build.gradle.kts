dbffa21ca7992153c14f3837b81527214f8af7ef
codeItRalf <raffe_83@hotmail.com>
2024-06-23 11:17:29 +0200

    Increase version name and code.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "app.codeitralf.radiofinder"
         minSdk = 24
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.1.1"
+        versionCode = 4
+        versionName = "1.1.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
